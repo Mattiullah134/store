87c9a3074d17338500eec9a033c41ff1ed853583
Mattiullah134 <117710678+Mattiullah134@users.noreply.github.com>
2023-03-02 22:04:56 +0500

    add new logo

--- components/Statics.js
+++ components/Statics.js
@@ -6,29 +6,27 @@ import rizwanRaja from '../assets/images/users/rizwanRaja.jpeg';
 import p2plogo from '../assets/images/logos/p2pCloudsLogo.png';
 import tepsLogo from '../assets/images/logos/tepsLogo.png';
 import pbiLogo from '../assets/images/logos/pbi.webp';
-import organicCure from '../assets/images/logos/organic_cure.png';
-import anjTechLogo from '../assets/images/logos/anjTech.png';
+import anzTechLogo from '../assets/images/logos/anz.webp';
 function Statics() {
     return (
         <section className="text-gray-600 body-font ">
             <div className="container px-5 py-5 mx-auto">
                 {/* <h1 className='text-center text-3xl sm:text-4xl md:text-6xl font-sans tracking-tight text-black font-semibold mb-2'>Beloved Partners</h1> */}
-                <div className="flex flex-wrap -m-4 text-center justify-center aligns-center mb-28">
-                    <div className="p-4 md:w-72 sm:w-3/4  ">
+                <div className="flex flex-wrap -m-4 text-center justify-center aligns-start mb-28 ">
+                    <div className="p-4 md:w-72 sm:w-1/2 object-contain  ">
                         <Image src={p2plogo} alt='Ahmad Manzoor' width={200} />
-                        {/* <img src={ahmadManzoor} alt='ahmad basit' /> */}
                     </div>
-                    <div className="p-2 md:w-72 sm:w-3/4 ">
+                    <div className="p-2 md:w-72 sm:w-1/2 object-contain ">
                         <Image src={tepsLogo} alt='Mian Ahmad Basit' width={200} />
                     </div>
-                    <div className="p-2 md:w-72 sm:w-3/4 ">
+                    <div className="p-2 md:w-72 sm:w-1/2 object-contain ">
                         <Image src={pbiLogo} width={200} alt='Rizan raja' />
                     </div>
-                    <div className="p-2 md:w-72 sm:w-3/4 ">
-                        <Image src={organicCure} width={200} alt='Rizan raja' />
-                    </div>
-                    <div className="p-2 md:w-72 sm:w-3/4 ">
-                        <Image src={anjTechLogo} width={200} alt='Rizan raja' />
+                    {/* <div className="p-2 md:w-72 sm:w-1/2 object-contain ">
+                        <Image src={organicCure} width={100} alt='Rizan raja' />
+                    </div> */}
+                    <div className="p-2 md:w-72 sm:w-1/2 object-contain ">
+                        <Image src={anzTechLogo} width={200} alt='Rizan raja' />
                     </div>
 
                 </div>
