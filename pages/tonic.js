9463de6be4291b82248934b7c65792d4a01897b5
Mattiullah134 <117710678+Mattiullah134@users.noreply.github.com>
2023-03-03 16:33:58 +0500

    delete the getproduct api

--- pages/tonic.js
+++ pages/tonic.js
@@ -6,6 +6,9 @@ import mongoose from "mongoose";
 
 function Tshirt({ product }) {
 
+
+
+
   return (
     <>
       <Head>
@@ -24,20 +27,20 @@ function Tshirt({ product }) {
         <div className="mx-auto min-h-screen max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
           <h2 className="sr-only">Products</h2>
 
-          {product.data.length === 0 && <div className="font-semibold text-center">Sorry! Currently Stock Unavailble right now. Please wait for the new Stock.!</div>}
+          {Object.keys(product).length === 0 && <div className="font-semibold text-center">Sorry! Currently Stock Unavailble right now. Please wait for the new Stock.!</div>}
           <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
 
-            {product.data.map((item) => {
-              return <div key={item._id} className="group relative">
+            {Object.keys(product).map((item) => {
+              return <div key={product[item]._id} className="group relative">
                 <div className="aspect-w-1 flex justify-center border aspect-h-1 w-full overflow-hidden rounded-lg bg-white xl:aspect-w-7 xl:aspect-h-8 ">
                   <img
-                    src={item.prodtImage}
+                    src={product[item].prodtImage}
                     className="h-full w-full object-contain group-hover:opacity-75"
                   />
                 </div>
-                <img src={item.img} className="w-10 absolute bottom-20 right-30" />
-                <h3 className="mt-4 text-sm text-gray-700">{item.title}</h3>
-                <p className="mt-1 text-lg font-medium text-gray-900">${item.price}</p>
+                <img src={product[item].img} className="w-10 absolute bottom-20 right-30" />
+                <h3 className="mt-4 text-sm text-gray-700">{product[item].title}</h3>
+                <p className="mt-1 text-lg font-medium text-gray-900">${product[item].price}</p>
               </div>
             })}
           </div>
@@ -49,16 +52,42 @@ function Tshirt({ product }) {
 
 
 export async function getServerSideProps() {
+  if (!mongoose.connections[0].readyState) {
+    mongoose.set("strictQuery", false);
+    await mongoose.connect(process.env.MONGO_URI)
+  }
+  let products = await Product.find({ category: 'tonic' })
+  let tshirts = {}
+  for (let item of products) {
+    if (item.title in tshirts) {
+      if (!tshirts[item.title].color.includes(item.color) && item.avilableQty > 0) {
+        tshirts[item.title].color.push(item.color)
+      }
+      if (!tshirts[item.title].size.includes(item.size) && item.avilableQty > 0) {
+        tshirts[item.title].size.push(item.size)
+      }
+    }
+    else {
+      // tshirts[item.title] is key and its value is whole object(item)
+      tshirts[item.title] = JSON.parse(JSON.stringify(item))
+      if (item.avilableQty > 0) {
+        tshirts[item.title].color = [item.color]
+        tshirts[item.title].size = [item.size]
+      }
+      else {
+        tshirts[item.title].color = []
+        tshirts[item.title].size = []
+      }
 
-  const data = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/getProduct`, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-  const jsonData = await data.json();
+    }
+  };
+
+
+
+
   // Pass data to the page via props
   return {
-    props: { product: jsonData }
+    props: { product: JSON.parse(JSON.stringify(tshirts)) }
   }
 }
 
